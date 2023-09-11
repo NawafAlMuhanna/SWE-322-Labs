cb439030ffd03251e9e1a747020630ee8c807de0
NawafAlMuhanna <98089702+NawafAlMuhanna@users.noreply.github.com>
2023-09-11 15:21:11 +0300

    Lab 01

---
+++ Lab01/nawafalmuhanna_lab1.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h1> Nawaf Al-Muhanna 202011016 </h1>";
+
+echo $n = 1 . "<br/>";
+echo $s = "1" . "<br/>";
+echo $n1 = $n . $s . "<br/>";
+echo $n2 = $n1 . " 10 little penguins" . "<br/>";
+echo $n3 = "hello" . "<br/>";
+echo $n4 = " world" . "<br/>";
+echo $n5 = $n3 . $n4 . "<br/>";
+echo $n6 = $n3 . $n4;
+
+?>
